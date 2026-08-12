dc5f4c81c2a8666c21e064728c3816e1db6405d1
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-08-12 13:07:34 +0800

    refactor: move helium input to browser aspect

--- modules/apps/browser.nix
+++ modules/apps/browser.nix
@@ -1,4 +1,11 @@
 { inputs, ... }: {
+  flake-file.inputs = {
+    helium = {
+      url = "github:oxcl/nix-flake-helium-browser";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
   den.aspects.apps.browser = {
     nixos = {
       imports = [
--- modules/flake-file.nix
+++ modules/flake-file.nix
@@ -5,16 +5,11 @@
     (inputs.den.flakeModules.dendritic or { })
   ];
 
-  # other inputs may be defined at a module using them.
   flake-file.inputs = {
     den.url = "github:denful/den";
     flake-file.url = "github:vic/flake-file";
     hjem = {
       url = "github:feel-co/hjem";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    helium = {
-      url = "github:oxcl/nix-flake-helium-browser";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
